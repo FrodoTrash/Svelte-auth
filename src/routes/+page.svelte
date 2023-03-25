3db1b1788936ae2936255b93e0fa1108f7ee614d
FrodoTrash <mheybal@gmail.com>
2023-03-26 00:51:46 +0100

    quick fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,12 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center">
-		<h2 class="font-bold">Welcome {data?.user?.username}.</h2>
+		{#if data?.user?.username }
+			<h2 class="font-bold">Welcome {data?.user?.username}.</h2>
+		{:else}
+			<h2 class="font-bold">You're not signed in</h2>
+		{/if}
+		
 		<!-- Animated Logo -->
 		<!-- / -->
 		<div class="flex justify-center space-x-2">
